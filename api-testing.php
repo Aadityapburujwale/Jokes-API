66f9d04b0b352d3b95f449c2080110168bf0246a
Aadityapburujwale <aadityapburujwale@gmail.com>
2022-04-29 09:00:38 +0530

    API Testing code added (cURL to make HTTP request), favicon added, .htaccess added to set home directory as api-testing.php

---
+++ api-testing.php
@@ -0,0 +1,33 @@
+<head> 
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.png">
+</head> 
+
+<?PHP 
+
+    $url="127.0.0.2/api.php?key=12&njokes=20";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    $result = json_decode($result,true);
+    
+        if($result['status']=='true'){
+                    
+                    forEach($result['jokes'] as $list){
+
+                        echo "<b>".$list['Joke']."</b><hr>";
+
+                    }
+        }else{
+            echo $result['response'];
+        }
+
+
+        // for($i=0;$i<25;$i++)
+        //     echo rand(0,100)." ";
+
+    // echo "<pre>";
+    // print_r($result);
+?>
