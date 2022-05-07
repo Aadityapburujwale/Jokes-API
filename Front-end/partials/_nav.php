fd3f248aa1cbf732310c9d03a8ae6dea440939a0
Aadityapburujwale <aadityapburujwale@gmail.com>
2022-05-07 13:07:00 +0530

    Navbar - links/url path fixed

--- Front-end/partials/_nav.php
+++ Front-end/partials/_nav.php
@@ -6,7 +6,7 @@ else{
   $loggedin = false;
 }
 echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/loginsystem">Jokes app</a>
+  <a class="navbar-brand" href="/Front-End">Jokes app</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -14,20 +14,20 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/loginsystem/welcome.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/Front-End/welcome.php">Home <span class="sr-only">(current)</span></a>
       </li>';
 
       if(!$loggedin){
       echo '<li class="nav-item">
-        <a class="nav-link" href="/loginsystem/login.php">Login</a>
+        <a class="nav-link" href="/Front-End/login.php">Login</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/loginsystem/signup.php">Signup</a>
+        <a class="nav-link" href="/Front-End/signup.php">Signup</a>
       </li>';
       }
       if($loggedin){
       echo '<li class="nav-item">
-        <a class="nav-link" href="/loginsystem/logout.php">Logout</a>
+        <a class="nav-link" href="/Front-End/logout.php">Logout</a>
       </li>';
     }
        
